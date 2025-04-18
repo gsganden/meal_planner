28ce16098e1e500a31cb5eef5a1f23e7bbbab714
gsganden <greg@gandenberger.org>
2025-04-18 15:41:39 -0500

    Uncomment test cases

--- tests/test_main_evals.py
+++ tests/test_main_evals.py
@@ -14,23 +14,23 @@ recipes = {
     "data/classic-deviled-eggs-recipe-1911032.html": Recipe(
         name="Classic Deviled Eggs"
     ),
-    # "data/good-old-fashioned-pancakes.html": Recipe(name="Good Old Fashioned Pancakes"),
-    # "data/skillet-chicken-parmesan-with-gnocchi.html": Recipe(
-    #     name="Skillet Chicken Parmesan with Gnocchi"
-    # ),
-    # "data/gochujang-sloppy-joes.html": Recipe(name="Gochujang Sloppy Joes"),
-    # "data/mushroom-pasta-creamy.html": Recipe(
-    #     name="Pasta ai Funghi (Creamy Pasta With Mushrooms)"
-    # ),
-    # "data/easy-bok-choy-recipe_.html": Recipe(name="Easy Bok Choy"),
-    # "data/sunshine-sauce-recipe-23706247.html": Recipe(name="Sunshine Sauce"),
-    # "data/quick-healthy-dinner-20-minute-honey-garlic-shrimp_.html": Recipe(
-    #     name="20 Minute Honey Garlic Shrimp"
-    # ),
-    # "data/prawn-salmon-burgers-spicy-mayo.html": Recipe(
-    #     name="Prawn & Salmon Burgers with Spicy Mayo"
-    # ),
-    # "data/easy-homemade-falafel-recipe_.html": Recipe(name="Easy Homemade Falafel"),
+    "data/good-old-fashioned-pancakes.html": Recipe(name="Good Old Fashioned Pancakes"),
+    "data/skillet-chicken-parmesan-with-gnocchi.html": Recipe(
+        name="Skillet Chicken Parmesan with Gnocchi"
+    ),
+    "data/gochujang-sloppy-joes.html": Recipe(name="Gochujang Sloppy Joes"),
+    "data/mushroom-pasta-creamy.html": Recipe(
+        name="Pasta ai Funghi (Creamy Pasta With Mushrooms)"
+    ),
+    "data/easy-bok-choy-recipe_.html": Recipe(name="Easy Bok Choy"),
+    "data/sunshine-sauce-recipe-23706247.html": Recipe(name="Sunshine Sauce"),
+    "data/quick-healthy-dinner-20-minute-honey-garlic-shrimp_.html": Recipe(
+        name="20 Minute Honey Garlic Shrimp"
+    ),
+    "data/prawn-salmon-burgers-spicy-mayo.html": Recipe(
+        name="Prawn & Salmon Burgers with Spicy Mayo"
+    ),
+    "data/easy-homemade-falafel-recipe_.html": Recipe(name="Easy Homemade Falafel"),
 }
 
 
