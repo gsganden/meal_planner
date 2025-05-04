202587707637bd0c6d5ab9eb53d7e6d5724840ab
gsganden <greg@gandenberger.org>
2025-05-04 12:55:36 -0500

    Clean up

--- deploy.py
+++ deploy.py
@@ -16,15 +16,12 @@ image = (
     .pip_install_from_pyproject("pyproject.toml")
     .add_local_python_source("meal_planner")
     .add_local_dir("prompt_templates", remote_path="/root/prompt_templates")
-    .add_local_dir("scripts", remote_path="/root/scripts")
 )
 
 google_api_key_secret = modal.Secret.from_dict(
     {"GOOGLE_API_KEY": os.environ["GOOGLE_API_KEY"]}
 )
 
-# Determine the expected data directory path inside the container
-# Assumes source code lands in /root/meal_planner
 APP_ROOT_IN_CONTAINER = Path("/root")
 DATA_DIR_IN_CONTAINER = APP_ROOT_IN_CONTAINER / "data"
 
@@ -37,7 +34,6 @@ def _check_api_key():
 @app.function(
     image=image,
     secrets=[google_api_key_secret],
-    # Mount the volume at the location the app code expects
     volumes={str(DATA_DIR_IN_CONTAINER): volume},
 )
 @modal.asgi_app()
