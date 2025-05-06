f359dfc9aa000fbca64308fecd6a63c520c26f8d
gsganden <greg@gandenberger.org>
2025-05-06 10:23:12 -0500

    Require 100% test coverage

--- run_fast_coverage.sh
+++ run_fast_coverage.sh
@@ -6,6 +6,7 @@ source .env
 uv run pytest \
     --cov=meal_planner \
     --cov-report term-missing \
+    --cov-fail-under=100 \
     tests/test_main.py \
     tests/test_api.py \
     'tests/test_main_evals.py::test_extract_recipe_name[tests/data/recipes/raw/good-old-fashioned-pancakes.html]' \
