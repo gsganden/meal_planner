703c73d062d4e052540bea26c102c2afda15d8db
gsganden <greg@gandenberger.org>
2025-05-07 09:06:49 -0500

    Delete comments

--- alembic/versions/1c2c47357c10_create_recipe_table.py
+++ alembic/versions/1c2c47357c10_create_recipe_table.py
@@ -12,7 +12,6 @@ import sqlalchemy as sa
 
 from alembic import op
 
-# revision identifiers, used by Alembic.
 revision: str = "1c2c47357c10"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
