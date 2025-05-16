961f14c3e1e5785ad240deb6caa763d1836d8676
gsganden <greg@gandenberger.org>
2025-05-16 15:55:24 -0500

    Move test

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,29 +19,26 @@ from meal_planner.main import (
     app,
 )
 from meal_planner.models import RecipeBase
+from tests.constants import (
+    FIELD_INGREDIENTS,
+    FIELD_INSTRUCTIONS,
+    FIELD_MODIFICATION_PROMPT,
+    FIELD_NAME,
+    FIELD_ORIGINAL_INGREDIENTS,
+    FIELD_ORIGINAL_INSTRUCTIONS,
+    FIELD_ORIGINAL_NAME,
+    FIELD_RECIPE_TEXT,
+    FIELD_RECIPE_URL,
+    RECIPES_EXTRACT_RUN_URL,
+    RECIPES_FETCH_TEXT_URL,
+    RECIPES_LIST_PATH,
+    RECIPES_MODIFY_URL,
+    RECIPES_SAVE_URL,
+)
 
 # Constants
 TRANSPORT = ASGITransport(app=app)
 TEST_URL = "http://test-recipe.com"
-
-# URLs
-RECIPES_LIST_PATH = "/recipes"
-RECIPES_EXTRACT_URL = "/recipes/extract"
-RECIPES_FETCH_TEXT_URL = "/recipes/fetch-text"
-RECIPES_EXTRACT_RUN_URL = "/recipes/extract/run"
-RECIPES_MODIFY_URL = "/recipes/modify"
-RECIPES_SAVE_URL = "/recipes/save"
-
-# Form Field Names
-FIELD_RECIPE_URL = "input_url"
-FIELD_RECIPE_TEXT = "recipe_text"
-FIELD_NAME = "name"
-FIELD_INGREDIENTS = "ingredients"
-FIELD_INSTRUCTIONS = "instructions"
-FIELD_MODIFICATION_PROMPT = "modification_prompt"
-FIELD_ORIGINAL_NAME = "original_name"
-FIELD_ORIGINAL_INGREDIENTS = "original_ingredients"
-FIELD_ORIGINAL_INSTRUCTIONS = "original_instructions"
 
 
 def _build_ui_fragment_form_data(
